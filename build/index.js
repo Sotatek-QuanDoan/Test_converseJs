6dbc7f73cf772c9a7ed947d0ee38c603ba5ab6c6
Quan.Doan <ba.hoang@blackgibbon.com>
2020-12-15 09:00:02 +0700

    test: new question

--- build/index.js
+++
@@ -1,1 +0,0 @@
-converse.initialize({bosh_service_url:"https://conversejs.org/http-bind/",show_controlbox_by_default:!0,whitelisted_plugins:["myplugin"],view_mode:"embedded"}),converse.plugins.add("myplugin",{initialize:function(){this._converse.api.listen.on("messageSend",(e=>{alert(e.attributes.message)}))}});
